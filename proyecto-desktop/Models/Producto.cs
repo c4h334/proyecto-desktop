79f664ab2a814ba16e0af71c7062f0e3f9e7d129
Anderson Jesús Monge Alvarado <anderson.monge@ucr.ac.cr>
2026-05-14 20:42:36 -0600

    Se agregró la conexión con la API y se implementó el crear, actualizar y eliminar productos sin datos hardcodeados y ahora utilizan la API.

--- proyecto-desktop/Models/Producto.cs
+++ proyecto-desktop/Models/Producto.cs
@@ -1,23 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace proyecto_desktop.Models
 {
     public class Producto
     {
+        [JsonPropertyName("productResourceId")]
+        public Guid? ProductResourceId { get; set; }
 
-
+        [JsonPropertyName("name")]
         public string Nombre { get; set; } = "";
+
+        [JsonPropertyName("description")]
         public string Descripcion { get; set; } = "";
+
+        [JsonPropertyName("quantity")]
         public int Cantidad { get; set; }
+
+        [JsonPropertyName("price")]
         public decimal Precio { get; set; }
+
+        [JsonPropertyName("code")]
         public string Codigo { get; set; } = "";
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; } = "";
+
+        [JsonPropertyName("available")]
         public bool Disponible { get; set; }
+
+        [JsonPropertyName("discount")]
         public decimal Descuento { get; set; }
+
+        [JsonPropertyName("discountQuantity")]
         public int CantDescuento { get; set; }
+
+        [JsonPropertyName("material")]
         public string Material { get; set; } = "";
     }
 }
