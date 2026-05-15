d108da3941d5e03da469b2016ad76f18fb893000
c4h334 <anderson.monge@ucr.ac.cr>
2026-05-14 21:02:17 -0600

    Revert "Feature/api connection"

--- proyecto-desktop/Models/Cliente.cs
+++ proyecto-desktop/Models/Cliente.cs
@@ -6,13 +6,7 @@ using System.Threading.Tasks;
 
 namespace proyecto_desktop.Models
 {
-    public class Cliente
+    internal class Cliente
     {
-        public string Nombre { get; set; } = "";
-        public string Apellidos { get; set; } = "";
-        public string Identificacion { get; set; } = "";
-        public string Tel { get; set; } = "";
-        public string DireccionCasa { get; set; } = "";
-        public string Correo { get; set; } = "";
     }
 }
